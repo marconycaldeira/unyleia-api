b2e09aea1d6ef9aa6462273eaef5076cdb3e021c
Marcony Caldeira <marcony@MacBook-Air-de-Marcony.local>
2020-08-18 19:47:43 -0300

    Configura rotas

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,17 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('books', 'API\\BooksController')->except(['create', 'show', 'create']);
+Route::resource('authors', 'API\\AuthorsController')->except(['create', 'show', 'create']);
+Route::resource('genres', 'API\\GenrersController')->except(['create', 'show', 'create']);
+Route::resource('publishes', 'API\\PublishesController')->except(['create', 'show', 'create']);
