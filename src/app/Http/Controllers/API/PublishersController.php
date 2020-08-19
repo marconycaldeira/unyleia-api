14b4f33d75f50fabe70159d7d1cce89b93a83aa9
Marcony Caldeira <marcony@MacBook-Air-de-Marcony.local>
2020-08-19 11:35:06 -0300

    Adiciona modulo de editoras

--- src/app/Http/Controllers/API/PublishersController.php
+++ src/app/Http/Controllers/API/PublishersController.php
@@ -2,86 +2,58 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Publisher;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePublishers;
+use App\Http\Requests\UpdatePublishers;
 
 class PublishersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $publishers = Publisher::get();
         return $publishers;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(StorePublishers $request)
     {
-        //
+        try{
+            $publisher = Publisher::create([
+                'name' => $request->name
+            ]);
+
+            return $publisher;
+        }catch(\Exception $e){
+            return response()->json('Erro ao salvar editora. Por favor, verifique os campos informados e tente novamente', 400);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function update(UpdatePublishers $request, $id)
     {
-        //
+        try{
+            $publisher = Publisher::find($id);
+            $publisher->update([
+                'name' => $request->name
+            ]);
+
+            return response()->json($publisher);
+        }catch(\Exception $e){
+            return response()->json('Erro ao salvar editora. Por favor, verifique os campos informados e tente novamente', 400);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+
+        try{
+            $publisher = Publisher::find($id);
+            $publisher->delete();
+
+            return response()->json($publisher);
+        }catch(\Exception $e){
+            return response()->json('Houve um erro ao tentar excluir, por gentileza, tente novamente', 400);
+        }
+
     }
 }
